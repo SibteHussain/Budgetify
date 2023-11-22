3ad85b0d66c5c8ad9a4382b97beb0484e5d7a9b0
SibteHussain <sibte566@gmail.com>
2023-11-22 18:14:06 +0500

    Improved user transactions page styling

--- src/screens/Beneficiaries/BeneficiaryTransactions.js
+++ src/screens/Beneficiaries/BeneficiaryTransactions.js
@@ -1,9 +1,11 @@
 import React, {useState, useEffect} from 'react';
-import {Text, FlatList} from 'react-native';
+import {Text, FlatList, StyleSheet} from 'react-native';
 import MainViewWrapper from '../../components/MainViewWrapper';
 import GeneralHeader from '../../components/GeneralHeader';
 import {useAppStateProvider} from '../../components/providers/AppStateProvider';
 import TransactionCard from '../../components/home/TransactionCard';
+import {widthPercentageToDP} from 'react-native-responsive-screen';
+import {View} from 'native-base';
 
 const BeneficaryTransactions = ({route}) => {
   const {id, navigate} = route.params;
@@ -33,6 +35,11 @@ const BeneficaryTransactions = ({route}) => {
   return (
     <MainViewWrapper statusBgColor={'#6947cc'}>
       <GeneralHeader bgColor={'#6947cc'} title={'TRANSACTIONS'} />
+      <View style={styles.topContainer}>
+        <View style={styles.textWrap}>
+          <Text style={styles.text}>Transactions by this user</Text>
+        </View>
+      </View>
       <FlatList
         data={filteredExpenses}
         renderItem={renderItem}
@@ -41,5 +48,25 @@ const BeneficaryTransactions = ({route}) => {
     </MainViewWrapper>
   );
 };
-
+const styles = StyleSheet.create({
+  text: {color: '#fff', fontFamily: 'inter_semibold', fontSize: 18},
+  topContainer: {
+    backgroundColor: '#6947cc',
+    width: widthPercentageToDP(100),
+    alignItems: 'center',
+    paddingBottom: '3%',
+  },
+  dateContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignContent: 'center',
+    justifyContent: 'space-around',
+    width: widthPercentageToDP(90),
+  },
+  textWrap: {
+    backgroundColor: '#431cb8',
+    padding: 15,
+    borderRadius: 15,
+  },
+});
 export default BeneficaryTransactions;
