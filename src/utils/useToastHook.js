6c33fcbbd7d279cbc9475a140a56f0140a61afc3
SibteHussain <sibte566@gmail.com>
2023-11-24 20:23:47 +0500

    Toast bug fixed

--- src/utils/useToastHook.js
+++ src/utils/useToastHook.js
@@ -1,5 +1,5 @@
 import {Text, useToast, View} from 'native-base';
-import React, {useEffect, useRef, useCallback} from 'react';
+import React, {useEffect, useRef} from 'react';
 
 import {widthPercentageToDP} from 'react-native-responsive-screen';
 
@@ -8,55 +8,53 @@ const useToastHook = (type, message, placement) => {
   const id = 'test-toast';
   const firstRender = useRef(true);
 
-  const showToast = useCallback(
-    (type, message, placement) => {
-      if (!toast.isActive(id)) {
-        toast.show({
-          id,
-          placement: placement === 'bottom' && 'top',
-          duration: 2000,
-          render: () => {
-            return (
-              <View
-                bg={type === 'error' ? '#eb343a' : '#6947cc'}
-                px="2"
-                py="1"
-                rounded="sm"
-                mb={2}
-                width={widthPercentageToDP('90')}
+  const showToast = (type, message, placement) => {
+    if (!toast.isActive(id)) {
+      toast.show({
+        id,
+        placement: placement === 'bottom' && 'top',
+        duration: 2000,
+        render: () => {
+          return (
+            <View
+              bg={type === 'error' ? '#eb343a' : '#6947cc'}
+              px="2"
+              py="1"
+              rounded="sm"
+              mb={2}
+              width={widthPercentageToDP('90')}
+              // height='20'
+              justifyContent="center"
+              alignItems="center"
+              borderRadius={50}>
+              <Text
+                fontFamily={'inter_semibold'}
+                color={'#fff'}
+                fontSize={15}
                 justifyContent="center"
                 alignItems="center"
-                borderRadius={50}>
-                <Text
-                  fontFamily={'MontSemiBold'}
-                  color={'#fff'}
-                  fontSize={18}
-                  justifyContent="center"
-                  alignItems="center"
-                  textAlign="center"
-                  flexWrap={'wrap'}
-                  numberOfLines={3}
-                  w={260}
-                  opacity={1}
-                  padding={2}>
-                  {message}
-                </Text>
-              </View>
-            );
-          },
-        });
-      }
-    },
-    [toast, id],
-  );
+                textAlign="center"
+                flexWrap={'wrap'}
+                numberOfLines={3}
+                w={260}
+                opacity={1}
+                padding={2}>
+                {message}
+              </Text>
+            </View>
+          );
+        },
+      });
+    }
+  };
 
   useEffect(() => {
-    if (!firstRender.current) {
-      showToast(type, message, placement);
+    if (!firstRender) {
+      showToast();
     } else {
       firstRender.current = false;
     }
-  }, [type, message, placement, showToast]);
+  }, [type, message, placement]);
 
   return {showToast};
 };
